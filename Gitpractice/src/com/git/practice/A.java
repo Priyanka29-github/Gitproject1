a09496b763d90041e19bccd905fb2ae3d4b8440c
Priyanka Pawar <priyankapawar2829@gmail.com>
2022-09-18 15:18:56 +0530

    first commit

---
+++ Gitpractice/src/com/git/practice/A.java
@@ -0,0 +1,18 @@
+package com.git.practice;
+
+import java.util.HashMap;
+import java.util.TreeSet;
+
+public class A {
+
+	public static void main(String[] args) {
+
+		HashMap hm = new HashMap();
+		hm.put(10, "ashok");
+		hm.put(11, "ram");
+
+		System.out.println(hm);
+
+	}
+
+}
---
+++ Gitpractice/src/com/git/practice/B.java
@@ -0,0 +1,15 @@
+package com.git.practice;
+
+import java.util.Hashtable;
+
+public class B {
+
+	public static void main(String[] args) {
+
+		Hashtable ht = new Hashtable();
+		ht.put(10, "ram");
+		ht.put(11, "sohan");
+		System.out.println(ht);
+	}
+
+}
