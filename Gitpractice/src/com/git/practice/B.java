f9518afdfb6a947ae07200c194268e310eee6937
Priyanka Pawar <priyankapawar2829@gmail.com>
2022-09-19 12:39:15 +0530

    remove function used

--- Gitpractice/src/com/git/practice/B.java
+++ Gitpractice/src/com/git/practice/B.java
@@ -9,6 +9,9 @@ public class B {
 		Hashtable ht = new Hashtable();
 		ht.put(10, "ram");
 		ht.put(11, "sohan");
+		ht.put(12, "kunal");
+		ht.put(13, "mrunal");
+		ht.remove(11);
 		System.out.println(ht);
 	}
 
